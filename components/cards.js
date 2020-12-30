9aa9df1fc54e0aa99d085af9a634114fdfab819b
Anthony Kuang <kuanganthony@gmail.com>
2020-12-30 07:33:51 -0800

    need to learn react states

--- components/cards.js
+++ components/cards.js
@@ -1,34 +1,34 @@
 import Card from './card';
 import styles from './cards.module.css';
 
-const sliders = document.querySelectorAll(`.${styles.cards}`);
-let isDown = false;
-let startX;
-let scrollLeft;
+// const sliders = document.querySelectorAll(`.${styles.cards}`);
+// let isDown = false;
+// let startX;
+// let scrollLeft;
 
-sliders.forEach((slider) => {
-  slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-  });
+// sliders.forEach((slider) => {
+//   slider.addEventListener('mousedown', (e) => {
+//     isDown = true;
+//     startX = e.pageX - slider.offsetLeft;
+//     scrollLeft = slider.scrollLeft;
+//   });
 
-  slider.addEventListener('mouseleave', () => {
-    isDown = false;
-  });
+//   slider.addEventListener('mouseleave', () => {
+//     isDown = false;
+//   });
 
-  slider.addEventListener('mouseup', () => {
-    isDown = false;
-  });
+//   slider.addEventListener('mouseup', () => {
+//     isDown = false;
+//   });
 
-  slider.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 3;
-    slider.scrollLeft = scrollLeft - walk;
-  });
-});
+//   slider.addEventListener('mousemove', (e) => {
+//     if (!isDown) return;
+//     e.preventDefault();
+//     const x = e.pageX - slider.offsetLeft;
+//     const walk = (x - startX) * 3;
+//     slider.scrollLeft = scrollLeft - walk;
+//   });
+// });
 
 export default function Cards() {
   return (
