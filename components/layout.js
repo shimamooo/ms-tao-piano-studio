9bd19f67bf85d8f0c60f8b65b73c487594e6ec0f
Anthony Kuang <kuanganthony@gmail.com>
2021-01-06 17:10:13 -0800

    commit

--- components/layout.js
+++ components/layout.js
@@ -14,6 +14,13 @@ export default function Layout({ children, page }) {
       <footer className={styles.footer}>
         <div className={utils.container}>
           <h3 className={utils.h3}>Ms. Tao Piano Studio</h3>
+          <div className={styles.contact}>
+            <a href='mailto:taopianostudio@gmail.com'>
+              <img src='img/email.svg' alt='email' />
+            </a>
+            <img src='img/phone.svg' alt='phone' />
+            <img src='img/wechat.svg' alt='wechat' />
+          </div>
           <div className={styles.container}>
             <ul className={`${utils.footerLink} ${styles.footerLinks}`}>
               <li>
@@ -43,13 +50,10 @@ export default function Layout({ children, page }) {
               </li>
             </ul>
             <div className={styles.footerRight}>
-              <div className={styles.contact}>
-                <img src='img/email.svg' alt='email' />
-                <img src='img/phone.svg' alt='phone' />
-                <img src='img/wechat.svg' alt='wechat' />
+              <div className={styles.copyright}>
+                <p>&copy; Ms. Tao Piano Studio</p>
+                <p>All rights reserved.</p>
               </div>
-              <p className={styles.copyright}>&copy; Ms. Tao Piano Studio</p>
-              <p className={styles.copyright}>All rights reserved.</p>
             </div>
           </div>
         </div>
