fe8fa9508f688007a238ecc0d88731d52b62439a
Anthony Kuang <kuanganthony@gmail.com>
2021-01-07 06:46:40 -0800

    auto image slider

--- components/image-slider.js
+++ components/image-slider.js
@@ -1,10 +1,37 @@
 import styles from './image-slider.module.css';
+import { useEffect } from 'react';
 
 export default function ImageSlider() {
+  useEffect(() => {
+    const slideshowImages = document.querySelectorAll(`.${styles.img}`);
+    let index = 0;
+
+    slideshowImages[index].style.opacity = 1;
+
+    setInterval(() => {
+      slideshowImages[index].style.opacity = 0;
+      index = (index + 1) % slideshowImages.length;
+      slideshowImages[index].style.opacity = 1;
+    }, 5000);
+  });
+
   return (
     <figure className={styles.imageSlider}>
-      <div className={styles.images}></div>
-      <div className={styles.sliderBar}></div>
+      <img
+        src='https://source.unsplash.com/random/1600x900'
+        className={styles.img}
+        alt=''
+      />
+      <img
+        src='https://source.unsplash.com/user/erondu/1600x900'
+        className={styles.img}
+        alt=''
+      />
+      <img
+        src='https://source.unsplash.com/collection/190727/1600x900'
+        className={styles.img}
+        alt=''
+      />
     </figure>
   );
 }
