4ead9c26e4ac3d35c244e83962ccb0efa825a548
Anthony Kuang <kuanganthony@gmail.com>
2021-01-06 19:18:27 -0800

    about section

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Layout from '../components/layout';
+import util from '../styles/utils.module.css';
+import styles from '../styles/About.module.css';
 
 export default function About() {
   return (
@@ -7,6 +9,32 @@ export default function About() {
       <Head>
         <title>About</title>
       </Head>
+      <main className={`${util.container} ${styles.content}`}>
+        <h1 className={util.h2}>About</h1>
+        <img src='img/about.jpg' alt='Ms. Tao and students' />
+        <p>
+          Children who study a musical instrument learn responsibility and
+          discipline, gain pride in their accomplishments which in turn makes
+          them more confident, and learn to think in a logical manner. Students
+          who study music often perform better in school. These are just a few
+          of the benefits that your child can gain.
+        </p>
+        <p>
+          The goal of the Ms. Tao Piano Studio is to provide an environment
+          suitable for learning piano and encouraging a love of music. Lesson
+          programs are designed to meet the specific needs of each student and
+          will be tailored to the level of your child’s ability. Chinese piano
+          music, Chinese folk songs will also be provided according to students’
+          and parents’ wishes. Also, Ms. Tao and her students hold annual piano
+          recitals every year. Fluent English, Cantonese and Mandarin are spoken
+          in classes.
+        </p>
+        <p>
+          If you have further questions, please feel free to contact Ms. Tao at
+          (925) 895-1393, leave a message or email taopianostudio@yahoo.com for
+          more information.
+        </p>
+      </main>
     </Layout>
   );
 }
