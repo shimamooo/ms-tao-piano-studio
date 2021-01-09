dc7f9cd93ccd22fb53f6a89ee4052840a1845acf
Anthony Kuang <kuanganthony@gmail.com>
2021-01-09 07:20:55 -0800

    polyFill

--- components/slide-down-button.js
+++ components/slide-down-button.js
@@ -1,9 +1,12 @@
 import styles from './slide-down-button.module.css';
 import smoothscroll from 'smoothscroll-polyfill';
-
-smoothscroll.polyfill();
+import { useEffect } from 'react';
 
 export default function SlideDownButton({ slide }) {
+  useEffect(() => {
+    smoothscroll.polyfill();
+  });
+
   const scroll = () => {
     const news = document.querySelector(`.${slide}`);
     news.scrollIntoView({ behavior: 'smooth' });
