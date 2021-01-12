7a29356898bf313b95a04d04fd4bba0526e3854d
Anthony Kuang <kuanganthony@gmail.com>
2021-01-11 18:47:32 -0800

    globals

--- pages/student-achievements.js
+++ pages/student-achievements.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Layout from '../components/layout';
+import utils from '../styles/utils.module.css';
 
 export default function StudentAchievements() {
   return (
@@ -7,6 +8,47 @@ export default function StudentAchievements() {
       <Head>
         <title>Student Achievements</title>
       </Head>
+      <main className={utils.container}>
+        <h1 className={utils.h2}>Student Achievements</h1>
+        <section>
+          <div>
+            <h2>2020</h2>
+          </div>
+          <div>
+            <h2>2019</h2>
+            <div>
+              <h3>Alex Ren</h3>
+              <p>
+                1st place - Won 2019 MTAC Alameda County East Piano Competition
+              </p>
+            </div>
+          </div>
+          <div>
+            <h2>2018</h2>
+          </div>
+          <div>
+            <h2>2017</h2>
+          </div>
+          <div>
+            <h2>2016</h2>
+          </div>
+          <div>
+            <h2>2015</h2>
+          </div>
+          <div>
+            <h2>2014</h2>
+          </div>
+          <div>
+            <h2>2013</h2>
+          </div>
+          <div>
+            <h2>2012</h2>
+          </div>
+          <div>
+            <h2>2011</h2>
+          </div>
+        </section>
+      </main>
     </Layout>
   );
 }
