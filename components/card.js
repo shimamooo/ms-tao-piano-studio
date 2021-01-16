4ec8d5856c3b99ee415b3e9dec56b1ad575ec48a
Anthony Kuang <kuanganthony@gmail.com>
2021-01-16 12:26:42 -0800

    media section complete

--- components/card.js
+++ components/card.js
@@ -1,7 +1,7 @@
 import styles from './card.module.css';
 import util from '../styles/utils.module.css';
 
-export default function Card({ year, section, img }) {
+export default function Card({ year, section, img, label, title, url }) {
   if (section === 'news') {
     return (
       <article className={styles.article}>
@@ -26,6 +26,27 @@ export default function Card({ year, section, img }) {
       </article>
     );
   } else {
-    return 'chill';
+    return (
+      <article className={styles.article}>
+        <a href='student-achievements#2019'>
+          <img
+            className={styles.img}
+            src={`img/${img}.jpg`}
+            alt={`${year} competition results`}
+          />
+        </a>
+        <p className={util.labelCards}>{label}</p>
+        <div className={styles.cardLink}>
+          <a href='student-achievements#2019' className={styles.link}>
+            <p>{title}</p>
+            <img
+              src='img/arrow-right.svg'
+              alt='arrow right'
+              className={styles.arrow}
+            />
+          </a>
+        </div>
+      </article>
+    );
   }
 }
--- components/cards.js
+++ components/cards.js
@@ -55,18 +55,90 @@ export default function Cards({ section }) {
   } else if (section === 'media') {
     return (
       <div className={styles.cards}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          label='Alex Ren'
+          title='Kabalevsky Op. 46 No. 3'
+          img='alex-ren'
+          url=''
+        />
+        <Card
+          label='Max Chan'
+          title='Thunder in the Dry Season'
+          img='max-chan'
+          url=''
+        />
+        <Card
+          label='Alex Ren & Ethan Yu'
+          title='Beethoven Marcia I & II'
+          img='alex-ren&ethan-yu'
+          url=''
+        />
+        <Card
+          label='Alex Ren & Ethan Yu'
+          title='Mozart Andante from Sonata K.381/123a in G Major'
+          img='alex-ren&ethan-yu2'
+          url=''
+        />
+        <Card
+          label='Anthony Kuang'
+          title="Menotti The Stranger's Dance"
+          img='anthony-kuang'
+          url=''
+        />
+        <Card
+          label='Erika Yang & Jessica Yuan'
+          title='Debussy Ballet from Petite Suite'
+          img='erika-yang&jessica-yuan'
+          url=''
+        />
+        <Card
+          label='Anthony Kuang'
+          title='Brown March'
+          img='anthony-kuang2'
+          url=''
+        />
+        <Card
+          label='Emily Guo & Lilly Jiang'
+          title='Liebeslider Waltz Op. 52a #9 in F Major'
+          img='lilly-jiang&emily-guo'
+          url=''
+        />
+        <Card
+          label='Emily Guo & Lilly Jiang'
+          title='Andre Sonatina Op. 45 #3 in F Major'
+          img='lilly-jiang&emily-guo2'
+          url=''
+        />
+        <Card
+          label='Jeffrey Tian'
+          title='Bloch Dream'
+          img='jeffrey-tian'
+          url=''
+        />
+        <Card
+          label='Stephanie Shao'
+          title='Lombaridze Evening on the Mountain'
+          img='stephanie-shao'
+          url=''
+        />
+        <Card
+          label='Alex Ren'
+          title='Bach Invention #9 in f minor'
+          img='alex-ren2'
+          url=''
+        />
+        <Card
+          label='Lilly Jiang'
+          title='Li Embroidery'
+          img='lilly-jiang'
+          url=''
+        />
+        <Card
+          label='Alex Ren'
+          title='Bartok Chromatic Invention, Mikrokosmos Vol 3 #92'
+          img='alex-ren3'
+          url=''
+        />
       </div>
     );
   }
